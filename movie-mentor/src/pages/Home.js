057e4b1ba93169aeebacc38bbeb5f6ce1f6f4549
Odera Okafor <okaforody000@gmail.com>
2023-09-23 13:44:16 -0400

    Home Components

---
+++ movie-mentor/src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { HomeContainer, BackgroundVideo, Content, LargeText, TransparentButton } from '../components/Navbar/InfoSection/HomeElements';
+
+const Home = () => {
+
+  return (
+    <HomeContainer>
+      <BackgroundVideo autoPlay muted loop>
+        <source src={require('../assets/.mp4')} type="video/mp4" />
+      </BackgroundVideo>
+
+      <Content>
+        <LargeText>Delve Into The Cinematic Gems</LargeText>
+        <div>
+          <Link to="/roadmaps">
+            <TransparentButton>ggg</TransparentButton>
+          </Link>
+          <Link to="/about">
+            <TransparentButton>ggg</TransparentButton>
+          </Link>
+        </div>
+      </Content>
+    </HomeContainer>
+  );
+};
+
+export default Home;
