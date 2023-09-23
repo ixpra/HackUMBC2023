c3081f3d9b49348a3830cfeda7d8f9b7bea9dc74
Sumedh Kane <33162468+sumedhkane03@users.noreply.github.com>
2023-09-23 13:42:29 -0400

    navbar setup

--- movie-mentor/src/components/Navbar.js
+++ movie-mentor/src/components/Navbar.js
@@ -1,9 +1,92 @@
-import React from 'react'
+import {React, useState, useRef, useEffect} from 'react'
 
-function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+import { Link, useNavigate } from 'react-router-dom';
+import {MdClose, MdMenu} from 'react-icons/md';
+
+import { onAuthStateChanged, getAuth, updateProfile, signOut } from "firebase/auth";
+
+
+import '../css/Navbar.css'
+import Alert from './Alert';
+
+const Navbar = () => {  
+
+    const navigate = useNavigate();
+    
+
+    
+
+    // setting up variables for window width and height
+    const [width, setWidth]   = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+    const updateDimensions = () => {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+    }
+
+    // setting up listener for window resize
+    useEffect(() => {
+        window.addEventListener("resize", updateDimensions);
+        return () => window.removeEventListener("resize", updateDimensions);
+    }, []);
+
+    
+         
+
+
+    
+
+    
+
+
+
+    return (
+        <>
+            {menu && (
+                <div className='menu-container menu-enter' id='cool-menu'>
+                    <MdClose className='close-menu' onClick={menuLeave}/>
+                    <Link to='/'><p className='menu-btn'>Home</p></Link>
+                    {/* <Link to='/blood-drive'><p className='menu-btn red-text-mobile'>Blood Drive</p></Link> */}
+
+                    <Link to='/about'><p className='menu-btn'>About us</p></Link>
+
+
+
+
+
+
+                    
+                </div>
+            )}
+
+            
+        
+        {width > 900 && (
+            <div className='nav' id='nav'>
+                <div className='top_logo' id='top_logo'>
+                <Link to='/'><div className='toplogo'><img src={require("../assets/hsslogo.png")} alt="logo" className='logo' id="small_topcorner"/ ></div></Link>
+                </div>
+                <div className='nav-buttons'>
+                    <Link to='/about'><p className='nav-btn'>About us</p></Link>
+                    
+                    
+                    
+                </div>
+            </div>
+        )}
+        {width < 900 && (
+            <div className='mobile-nav' id='mobile-nav'>
+                <div className='top_logo' id='top_logo'>
+                <Link to='/'><img src={require("../assets/cornerlogo.png")} alt="logo" className='logo' id="small_topcorner"/ ></Link>
+                </div>
+
+                <MdMenu className='open-menu' onClick={()=>{setMenu(true)}}/>
+            </div>
+        )}
+
+        </>
+    )
 }
 
-export default Navbar
+export default Navbar;
