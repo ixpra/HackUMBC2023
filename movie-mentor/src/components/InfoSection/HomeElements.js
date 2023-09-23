fa778179eab156183816abceac8e56f33c80fa09
Sumedh Kane <33162468+sumedhkane03@users.noreply.github.com>
2023-09-23 14:55:47 -0400

    wfq

--- movie-mentor/src/components/InfoSection/HomeElements.js
+++ movie-mentor/src/components/InfoSection/HomeElements.js
@@ -8,14 +8,12 @@ export const HomeContainer = styled.div`
 export const BackgroundVideo = styled.video`
   position: relative;
   top: 40px;
-  position: fixed;
-  top: 0;
   left: 0;
   width: 100%;
-  height: 150%;
+  height: 100%;
   /* object-fit: cover; */
   z-index: -1;
-  background: rgba(0, 0, 0, 0.5);
+  background: rgba(0, 0, 0, 0.5); 
 `;
 
 
