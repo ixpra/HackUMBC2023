b0dc684fff7d8cf7392d7fc3a58bb697f2c9d5b1
Sumedh Kane <33162468+sumedhkane03@users.noreply.github.com>
2023-09-24 10:18:23 -0400

    ekfjakfk

--- movie-mentor/src/components/MovieCard.js
+++ movie-mentor/src/components/MovieCard.js
@@ -4,6 +4,8 @@ import {FaInstagram, FaLinkedin, FaGithub} from 'react-icons/fa'
 import {BsWhatsapp,BsTwitter,BsFacebook} from 'react-icons/bs'
 
 
+import '../styles/About.css'
+
 
 
 const MovieCard = ({title,description}) => {
@@ -11,8 +13,8 @@ const MovieCard = ({title,description}) => {
     <div className='member-card-container' id={''}>
         {/* {picture} */}
         <h1 className='member-name'>{title}</h1>
-        {/* <p className='member-role'>{role}</p> */}
-        <p className='member-major'>{description}</p>
+        <p className='member-role'>{description}</p>
+        {/* <p className='member-major'>{description}</p> */}
         
     </div>
   )
